65885b83bfd8c7bca27dde5c719a0118d9053c4a
Alex Sieke <alexsieke@stripe.com>
2020-03-24 11:05:09 -0400

    finished auth

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import React from "react";
 import useFormValidation from "./useFormValidation";
 import validateLogin from "./validateLogin";
-import firebase from "../../firebase/firebase";
+import firebase from "../../firebase";
+import { Link } from "react-router-dom";
 
 const INITIAL_STATE = {
   name: "",
@@ -19,80 +19,81 @@ function Login(props) {
     errors,
     isSubmitting
   } = useFormValidation(INITIAL_STATE, validateLogin, authenticateUser);
-
-  const [login, setLogin] = useState(false);
+  const [login, setLogin] = React.useState(true);
+  const [firebaseError, setFirebaseError] = React.useState(null);
 
   async function authenticateUser() {
     const { name, email, password } = values;
-    const response = login
-      ? firebase.login(email, password)
-      : firebase.register(name, email, password);
-    console.log({ response });
+    try {
+      login
+        ? await firebase.login(email, password)
+        : await firebase.register(name, email, password);
+      props.history.push("/");
+    } catch (err) {
+      console.error("Authentication Error", err);
+      setFirebaseError(err.message);
+    }
   }
 
   return (
     <div>
       <h2 className="mv3">{login ? "Login" : "Create Account"}</h2>
-      <form className="flex flex-column" onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="flex flex-column">
         {!login && (
           <input
-            type="text"
-            placeholder="Your Name"
-            autoComplete="off"
             onChange={handleChange}
-            onBlur={handleBlur}
             value={values.name}
             name="name"
+            type="text"
+            placeholder="Your name"
+            autoComplete="off"
           />
         )}
         <input
-          type="text"
-          placeholder="Your Email"
-          autoComplete="off"
           onChange={handleChange}
           onBlur={handleBlur}
           value={values.email}
-          className={errors.email && "error-input"}
           name="email"
+          type="email"
+          className={errors.email && "error-input"}
+          placeholder="Your email"
+          autoComplete="off"
         />
         {errors.email && <p className="error-text">{errors.email}</p>}
         <input
-          type="password"
-          placeholder="Choose a secure password"
-          autoComplete="off"
           onChange={handleChange}
           onBlur={handleBlur}
           value={values.password}
           className={errors.password && "error-input"}
           name="password"
+          type="password"
+          placeholder="Choose a secure password"
         />
         {errors.password && <p className="error-text">{errors.password}</p>}
+        {firebaseError && <p className="error-text">{firebaseError}</p>}
         <div className="flex mt3">
           <button
             type="submit"
             className="button pointer mr2"
             disabled={isSubmitting}
+            style={{ background: isSubmitting ? "grey" : "orange" }}
           >
-            {login ? "login" : "create account"}
+            Submit
           </button>
-          <StyledLink onClick={() => setLogin(prevLogin => !prevLogin)}>
+          <button
+            type="button"
+            className="pointer button"
+            onClick={() => setLogin(prevLogin => !prevLogin)}
+          >
             {login ? "need to create an account?" : "already have an account?"}
-          </StyledLink>
+          </button>
         </div>
       </form>
+      <div className="forgot-password">
+        <Link to="/forgot">Forgot Password?</Link>
+      </div>
     </div>
   );
 }
-
-const StyledLink = styled.p`
-  border: 0px;
-  padding: 0px;
-  line-height: 14px;
-  font-size: 12px;
-  &:hover {
-    text-decoration: underline;
-    cursor: pointer;
-  }
-`;
 
 export default Login;
